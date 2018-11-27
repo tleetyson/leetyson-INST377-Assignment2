75d8d7589662374c58175c14ae753295e875e5c7
tleetyson <42789785+tleetyson@users.noreply.github.com>
2018-11-27 16:15:47 -0500

    Add files via upload

---
+++ form_processor.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Team Peer Evaluation - Processor</title>
+
+	<style>
+		div {
+			margin-top: 20px;
+			margin-bottom: 20px;
+		}
+	</style>
+</head>
+<body>
+
+<?php
+function individGrad($grades, $participation ) {
+    if( $participation == "Absent") {
+			return 55;
+		}
+		elseif ($participation == "Above and Beyond") {
+			return $grades;
+		}
+		elseif($participation == "Satisfactory"){
+			return $grades * .85;
+		}
+		else {
+			return $grades *.70;
+	}
+}
+
+$grades = $_POST["grades"];
+$minutes = round(abs(strtotime($_POST[project_end]) - strtotime($_POST[project_start])) / 60,2);
+echo "<h2> Team Project Results </h2>";
+echo "Team Project Name: " . $_POST["project_name"] . "<br>";
+echo "Total Time (minutes): " . $minutes . "<br>";
+echo "Overall Grade: " . $_POST["grades"] . "<br>";
+echo "Grade for " . $_POST["team_member1"] ." " . individGrad($grades, $_POST['participation1']) . "<br>";
+echo "Grade for " . $_POST["team_member2"] ." " . individGrad($grades, $_POST["participation2"]) . "<br>";
+echo "Grade for " . $_POST["team_member3"] ." " . individGrad($grades, $_POST["participation3"]) . "<br>";
+echo "Grade for " . $_POST["team_member4"] ." " . individGrad($grades, $_POST["participation4"]) . "<br>";
+echo "Grade for " . $_POST["team_member5"] ." " . individGrad($grades, $_POST["participation5"]) . "<br>";
+echo "<p>" . $_POST["comments"] . "</p>";
+
+
+
+
+
+
+
+?>
+</body>
+</html>
